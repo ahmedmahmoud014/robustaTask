59d6cc889f67169d9ccd333bebf777adb73c59a4
ahmed M. Alsaied <aalsaied.eg@stcs.com.sa>
2021-04-03 14:32:10 +0200

    AI-4.0.1 <stcs@STCSs-MacBook-Pro.local Overwrite remote https://github.com/ahmedmahmoud014/robustaTask.git to local

--- app/src/main/java/com/android/androidcodebase/utils/callback/LoadedCityCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.android.androidcodebase.utils.callback;
-
-import com.android.androidcodebase.data.model.Weather;
-import com.android.androidcodebase.data.model.WeatherInfo;
-
-public interface LoadedCityCallBack {
-
-    void loadedCityAction (WeatherInfo weatherInfo);
-}
--- app/src/main/java/com/android/androidcodebase/utils/enums/FragmentsEnum.java
+++
@@ -1,5 +0,0 @@
-package com.android.androidcodebase.utils.enums;
-
-public enum FragmentsEnum {
-    INITIAL,City,WEATHER,ALL_LOADED_WEATHER
-}
